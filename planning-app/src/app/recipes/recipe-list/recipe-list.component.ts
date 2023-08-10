874e0498fa8d69179154861a1069e5b86d5294be
bergony.bandeira <bergony.bandeira@quark.tec.br>
2023-08-09 21:59:35 -0300

    Add Event To recipe e shopping list selecet

--- planning-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ planning-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -14,9 +14,15 @@ export class RecipeListComponent {
       'https://www.simplyrecipes.com/thmb/4_wTLA6Q_2o7dgNO-ze0w7oeX_w=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1b-ea13798d224048b3a28afb0936c9b645.jpg'
     ),
     new Recipe(
-      'A teste recipe',
+      'Another   teste recipe',
       'this image tests',
       'https://www.simplyrecipes.com/thmb/4_wTLA6Q_2o7dgNO-ze0w7oeX_w=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1b-ea13798d224048b3a28afb0936c9b645.jpg'
     ),
   ];
+
+  @Output() recipeWasSeleted = new EventEmitter<Recipe>();
+
+  onRecipeSeleted(recipeEl: Recipe) {
+    this.recipeWasSeleted.emit(recipeEl);
+  }
 }
